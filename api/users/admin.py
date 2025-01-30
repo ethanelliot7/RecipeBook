3b3231132efe6a45a293aaef22b1bc4b668d0ec0
ethanelliot7 <ethanwaelliot@gmail.com>
2025-01-30 18:38:37 +1300

    added admin support for Profile model.

--- api/users/admin.py
+++ api/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ('bio','profile_picture')
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('bio','profile_picture')}),
+    )
+
+
